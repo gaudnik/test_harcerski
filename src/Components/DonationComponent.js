b7130540d95f4a45c0750399c82f17cc1b6a4688
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2023-06-21 13:45:51 +0200

    donation

--- src/Components/DonationComponent.js
+++ src/Components/DonationComponent.js
@@ -27,19 +27,22 @@ const DonationComponent = () => {
 
   return (
     <div className='donation'>
-      <h2 className='donation__header'>Wesprzyj nas datkiem</h2>
-      <p className='donation__text'>Wybierz jedną z kwot lub wpisz inną:</p>
-      <div className='donation__amounts'>
-        <button className='donation__amount'onClick={() => setDonationAmount('10')}>10 zł</button>
-        <button className='donation__amount' onClick={() => setDonationAmount('20')}>20 zł</button>
-        <button className='donation__amount' onClick={() => setDonationAmount('50')}>50 zł</button>
-        <button className='donation__amount' onClick={() => setDonationAmount('100')}>100 zł</button>
+      <div className='donation__section'>
+        <h2 className='donation__header'>Wesprzyj nas datkiem</h2>
+        <p className='donation__text'>Wybierz jedną z kwot lub wpisz inną:</p>
+        <div className='donation__amounts'>
+          <button className='donation__amount'onClick={() => setDonationAmount('10')}>10 zł</button>
+          <button className='donation__amount' onClick={() => setDonationAmount('20')}>20 zł</button>
+          <button className='donation__amount' onClick={() => setDonationAmount('50')}>50 zł</button>
+          <button className='donation__amount' onClick={() => setDonationAmount('100')}>100 zł</button>
+        </div>
+        <div className='donation__input-btn-box'>
+          <input className='donation__input' type="number" placeholder="Inna kwota" value={donationAmount} onChange={handleAmountChange} />
+          {error && <p style={{ color: 'red', marginBottom:'1rem' }}>{error}</p>}
+          <button className='btn donation__btn' onClick={handleDonation}>Wesprzyj</button>
+        </div>
       </div>
-      <div className='donation__input-btn-box'>
-        <input className='donation__input' type="number" placeholder="Inna kwota" value={donationAmount} onChange={handleAmountChange} />
-        {error && <p style={{ color: 'red', marginBottom:'1rem' }}>{error}</p>}
-        <button className='btn donation__btn' onClick={handleDonation}>Wesprzyj</button>
-      </div>
+      <div className='donation__background'></div>
     </div>
   );
 };
